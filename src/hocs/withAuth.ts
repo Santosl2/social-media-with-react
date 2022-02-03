c65af0757e005b25e715b2bb91bbccec676d6b4b
Santosl2c <mfilype2019@gmail.com>
2022-02-02 22:47:34 -0300

    feat: HOC protected route

---
+++ src/hocs/withAuth.ts
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from "next";
+import { supabase } from "../utils/supabase";
+
+const withAuth: (inner?: GetServerSideProps) => GetServerSideProps = (
+  inner
+) => {
+  return async (context) => {
+    const { req } = context;
+    const { user } = await supabase.auth.api.getUserByCookie(req);
+
+    if (!user) {
+      return { props: {}, redirect: { destination: "/" } };
+    }
+
+    if (inner) {
+      return inner(context);
+    }
+
+    return { props: {} };
+  };
+};
+
+export default withAuth;
