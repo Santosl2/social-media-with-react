dc48cf1486bc5f70a2b8b5b9732bf47a1530f119
Santosl2c <mfilype2019@gmail.com>
2022-02-03 20:18:02 -0300

    release: on publish a new post automatic update

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@chakra-ui/react";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 import { Posts } from "../components/Posts";
 import withAuth from "../hocs/withAuth";
 import { api } from "../services/api";
+import { supabase } from "../utils/supabase";
 
 export type Post = {
   id?: string;
@@ -19,6 +21,20 @@ type Props = {
 };
 
 export default function Dashboard({ postsData }: Props): JSX.Element {
+  const [newPosts, setNewPosts] = useState<boolean>(false);
+
+  // automatic update on insert a new post
+  useEffect(() => {
+    supabase
+      .from("posts")
+      .on("INSERT", (data) => {
+        const { new: newPost } = data;
+        postsData.data.unshift(newPost);
+        setNewPosts((prev) => !prev);
+      })
+      .subscribe();
+  }, []);
+
   return (
     <>
       <Header />
